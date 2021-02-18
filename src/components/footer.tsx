2a8e921cc0222749ad27c7fb11bf737696a396df
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2021-02-19 05:31:42 +0900

    レスポンシブ対応

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,34 +5,12 @@ import contactStyles from '../styles/contact.module.css'
 import GitHub from '../components/svgs/github'
 import Twitter from '../components/svgs/twitter'
 
-const contacts = [
-  {
-    Comp: Twitter,
-    alt: 'twitter icon',
-    link: 'https://twitter.com/Hosso_remark',
-  },
-  {
-    Comp: GitHub,
-    alt: 'github icon',
-    link: 'https://github.com/SakouHitoshi',
-  },
-]
-
 export default props => (
   <>
     <footer className={sharedStyles.withlove}>
       <div className="sharedStyles.thanks">
         POWERED BY {'  '}
         <a href="https://notion-blog.now.sh/">Notion Blog</a>
-      </div>
-      <div className={contactStyles.links}>
-        {contacts.map(({ Comp, link, alt }) => {
-          return (
-            <ExtLink key={link} href={link} aria-label={alt}>
-              <Comp height={32} />
-            </ExtLink>
-          )
-        })}
       </div>
     </footer>
   </>
