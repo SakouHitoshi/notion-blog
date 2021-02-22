b4ef7b6a6959d2a5b1c128f2a94f0b2dd1443767
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2021-02-23 05:23:47 +0900

    サムネ表示ver25

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,10 +20,17 @@ export default ({ titlePre = '' }) => {
         <title>{titlePre ? `${titlePre}` : ''} </title>
         <meta name="description" />
         <meta name="og:title" content="Hosso Blog" />
-        <meta property="og:image" content="/public/og-image.png" />
-        <meta name="twitter:site" content="@_ijjk" />
+        <meta property="og:url" content="https://hosso-blog/blog.jp" />
+        <meta
+          property="og:image"
+          content="https://notion-blog/public/og-image"
+        />
+        <meta name="twitter:site" content="@Hosso_remark" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/public/og-image.png" />
+        <meta
+          name="twitter:image"
+          content="https://notion-blog/public/og-image"
+        />
       </Head>
       <ul>
         {navItems.map(({ label, page, link }) => (
