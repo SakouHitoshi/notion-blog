4156461aa24192c5cd04067234c2871ae4f25b96
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2021-02-23 13:30:47 +0900

    Google Analyticsa導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,27 @@
 import '../styles/global.css'
 import 'katex/dist/katex.css'
 import Footer from '../components/footer'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-export default ({ Component, pageProps }) => (
-  <>
-    <Component {...pageProps} />
-    <Footer />
-  </>
-)
+export default ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      if (location.host === 'hosso-blog.jp') {
+        gtag.pageview(url)
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
+}
